a270e7179bdf5f3e10e4f63dab4ed8557dc14bb7
lageese <151532503+lageese@users.noreply.github.com>
2024-10-14 14:11:29 +0200

    Update p2.c

--- P2/p2.c
+++ P2/p2.c
@@ -3,13 +3,28 @@
 //para cada algoritmo va haber 3 tablas en el informe
 //proporcionan lass funciones de semilla, aleatorio y ascendente, desas funciones hay que hacer la FUNCIÓN SEMILLA
 
+
+//En esta práctica se van a implementar los algoritmos de inserción y de ordenación rápida con selección aleatoria del pivote.
+
+//La práctica consiste en ordenar un vector de n numeros enteros
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 
 
-//CÓDIGO DE LOS ALGORITMOS
+
+
+//CÓDIGOS PROPORCIONADOS EN LA PRÁCTICA
+//CÓDIGO DE LOS ALGORITMOS DE INSERCIÓN Y DE ORDENACIÓN RÁPIDA
+
+//FUNCIONES PROGRAMA
 void ordenaciónInsercion(char v[]){
 	int i,n,x,j;
 
@@ -24,6 +39,21 @@ void ordenaciónInsercion(char v[]){
 		v[j+1]=x;
 	}
 }
+
+/*void ord_ins (int v [], int n) {
+	int i, j,x;
+
+
+	for(i=2;i<=n;i++){
+		x = v[i];
+		j = j-1;
+		while (j>0 && v[j]>x){
+			v[j+1] = v[j];
+			j=j-1;;
+		}
+		v[j+1]=x;
+	}
+}*/
 
 
 
@@ -61,19 +91,13 @@ void ordenaciónAuxiliar(char v[]){
 	}
 }
 
-
-//CÓDIGOS PROPORCIONADOS EN LA PRÁCTICA
-
-
-void ord_ins (int v [], int n) {
-/* ... */
-}
 void ord_rap_aux (int v [], int iz, int dr) {
 /* ... */
 }
 void ord_rap (int v [], int n) {
 ord_rap_aux(v, 0, n-1);
 }
+
 
 
 void inicializar_semilla() {
@@ -86,11 +110,33 @@ for (i=0; i < n; i++)
 v[i] = (rand() % m) - n;
 }
 
+
+
 void ascendente(int v [], int n) {
 int i;
 for (i=0; i < n; i++)
 v[i] = i;
 }
+
+void descendente(int v[], int n){
+	int i,j=0;
+	for(i=0;i<n;i--){
+		v[i-1]=j;
+
+	}
+
+}
+
+
+void printVector(int v[], int n){
+	int i;
+	printf("[");
+	for(i=0;i<n;i++){
+		printf("%2d",v[i]);
+	}
+	printf("]\n");
+}
+
 
 
 //FUNCIÓN VOID TEST
@@ -105,6 +151,27 @@ void test(){
 
 
 //FUNCIONES PARA LAS MEDICIONES (para cada algoritmo va haber 3 tablas en el informe)
+
+/*inicializar(vector);
+leer_tiempo (ta); alg(vector); leer_tiempo (tb);
+t:=tb-ta;
+si (t < 500) entonces { % ‘‘umbral de confianza’’
+leer_tiempo (ta);
+repetir K veces: {
+inicializar(vector); alg(vector)
+};
+leer_tiempo (tb);
+t1:=tb-ta; % deber´ıa estar por encima de 500!
+leer_tiempo (ta);
+repetir K veces: % debe ser la misma constante K
+inicializar(vector);
+leer_tiempo (tb);
+t2:=tb-ta;
+t:=(t1-t2)/K
+}*/
+
+
+
 
 
 
