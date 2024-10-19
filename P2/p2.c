5a075849086447737978688030d00c0c9f7a339c
Xabi <114141049+Xabiguitian@users.noreply.github.com>
2024-10-19 18:22:33 +0200

    Update p2.c

--- P2/p2.c
+++ P2/p2.c
@@ -20,35 +20,31 @@
 #include <math.h>
 
 
-#define K 10000
+#define K 100000
 #define UMBRAL 1
 
 typedef enum cota{
-	CONST, SOBR, SUBS
+    CONST, SOBR, SUBS
 
 }cota;
 
 typedef struct tCota{
-	double resultado;
-	char *string;
+    double resultado;
+    char *string;
 
 }tCota;
 
 
+double microsegundos() {
+	struct timeval t;
+	if (gettimeofday(&t, NULL) < 0 )
+		return 0.0;
+	return (t.tv_usec + t.tv_sec * 1000000.0);
+}
 
 void inicializar_semilla() {
 	srand(time(NULL));
 }
-
-
-//conseguimos la hora actual del sistema en microsegundos--
-double microsegundos() {
-struct timeval t;
-if (gettimeofday(&t, NULL) < 0 )
-return 0.0;
-return (t.tv_usec + t.tv_sec * 1000000.0);
-}
-
 
 void aleatorio(int v [], int n) {/* se generan números pseudoaleatorio entre -n y +n */
 	int i, m=2*n+1;
@@ -215,11 +211,6 @@ void test(void (*algoritmo)(int[], int), int tamV){
 
 //FUNCIONES PARA LAS MEDICIONES (para cada algoritmo va haber 3 tablas en el informe)
 
-
-//FUNCIONES DE MEDIR EL TIEMPO
-
-//FUNCIONES PARA IMPRIMIR LAS TABLAS DE TIEMPOS
-
 /*inicializar(vector);
 leer_tiempo (ta); alg(vector); leer_tiempo (tb);
 t:=tb-ta;
@@ -238,225 +229,131 @@ t2:=tb-ta;
 t:=(t1-t2)/K
 }*/
 
+double tiempo(void (*algoritmo)(int[], int), int vector [], int tam){
 
+    double t1, t=0,t2,aux;
+    int i;
 
-double tiempo(void (*algoritmo)(int[], int), void (*vec)(int [], int tam), int tam){
+    t1=microsegundos();
+    algoritmo(vector,tam);
+    t2=microsegundos();
+    t=t2-t1;
+    if(t<500){
+        t1=microsegundos();
+        for(i=0;i<K;i++){
+            algoritmo(vector,tam);
+        }
+        t2=microsegundos();
+        t=t2-t1;
+        t=t/K;
+    }
 
-	double t1, t=0,t2,aux;
-	int *v;
-	int i;
-
-
-	v= malloc(tam *sizeof(int));
-	vec(v, tam);
-
-	t1=microsegundos();
-	algoritmo(v,tam);
-	t2=microsegundos();
-	t=t2-t1;
-	if(t<500){
-		t1=microsegundos();
-		for(i=0;i<K;i++){
-			vec(v,tam);
-			algoritmo(v,tam);
-		}
-		t2=microsegundos();
-		aux=t2-t1;
-		t1=microsegundos();
-		for(i=0;i<K;i++){
-			vec(v,tam);
-		}
-		t2=microsegundos();
-		t=t2-t1;
-		t=(aux-t)/K;
-	}
-
-
-free (v);
 return t;
 
 }
 
 
-int AscDescAleat(int v[], int n){
-int i;
-for(i=0; i<n;i++){
+void printTablasTiemposInsercion(){
+	int * desc, * asc, * aleat;
 
-	if(v[i] == i+1){ //es ascendente
-		return 1;
-	}else if(v[i] == n - i){ //es descendente
-		return 2;
-	}
-	}
-	return 3;
-}
+	desc=malloc(500 * sizeof(int));
+	asc=malloc(500 * sizeof(int));
+	aleat=malloc(500 * sizeof(int));
+	descendente(desc, 500);
+	ascendente(asc, 500);
+	aleatorio(aleat, 500);
 
 
-
-tCota CalcularCotaRap(int n , enum cota tipo,int AscDescAleat){
-	tCota c;
-	c.resultado=0;
-
-	if(AscDescAleat == 1){
-
-		if(tipo==SUBS){
-			c.resultado= pow (n,0.8);
-			c.string= (UMBRAL==1)? "____[t(n)/n^0.8]____" : "[t(n)/n^2.2]____";
-
-
-
-		}else if(tipo==CONST){
-			c.resultado= (UMBRAL==1)? pow(n,0.95)*log2(n) :
-			 (UMBRAL==10)? pow(n,0.99)*log2(n) : pow(n,1.06)*log2(n);
-			c.string= (UMBRAL==1)? "[t(n)/n^0.95 * log2 n]____" : 
-			(UMBRAL==10)? "[t(n)/n^0.99 * log2 n]____" : "[t(n)/n^2.2 * log2 n]____";
-			
-
-		}else if(tipo==SOBR){
-			c.resultado= (UMBRAL==1 || UMBRAL ==10) ? pow(n,1.2) :pow(n,1.3);
-			c.string= (UMBRAL==1 || UMBRAL ==10) ? "[t(n)/n^1.2]____" : "[t(n)/n^1.3]____";
-			
-		}
-
-
-
-	}else if(AscDescAleat == 2){
-
-		if(tipo==SUBS){
-			c.resultado=n ;
-			c.string= "____[t(n)/n]____";
-
-		}else if(tipo==CONST){
-			c.resultado= (UMBRAL==1) ? pow(n,1.06) : pow(n,1.1);
-			c.string= (UMBRAL==1) ? "[t(n)/n^1.06____]" : "[t(n)/n^1.1]____";;
-
-		}else if(tipo==SOBR){
-			c.resultado=pow(n,1.3) ;
-			c.string= "[t(n)/n^1.3]____";
-		}
-
-
-
-	}else{
-
-		if(tipo==SUBS){
-			c.resultado=n ;
-			c.string="____[t(n)/n]____]" ;
-
-		}else if(tipo==CONST){
-			c.resultado=(UMBRAL==1) ? pow(n, 1.01) : (UMBRAL==10) ? pow(n,1.14): pow(n,1.1);
-			c.string=(UMBRAL==1)? "[t(n)/n^1.01]____" :(UMBRAL==10)? "[t(n)/n^1.14]____" : "[t(n)/n^1.1]____";
-
-		}else if(tipo==SOBR){
-			c.resultado= pow(n,1.4);
-			c.string="[t(n)/n^1.4]____" ;
-		}
-
-
-
-	}
-	return c;
-
-}
-
-tCota CalcularCotaInser(int n, enum cota tipo ,int AscDescAleat ){
-	tCota c;
-	c.resultado=0;
-
-	if(AscDescAleat == 1){
-		if(tipo==SUBS){
-			c.resultado=pow(n,0.8) ;
-			c.string="____[t(n)/n^0.8]____" ;
-
-		}else if(tipo==CONST){
-			c.resultado= n;
-			c.string= "[t(n)/n]____";
-
-		}else if(tipo==SOBR){
-			c.resultado=pow(n,1.2) ;
-			c.string= "[t(n)/n^1.2]____";
-
-		}
-
-
-	}else if(AscDescAleat == 2){
-
-		if(tipo==SUBS){
-			c.resultado=pow(n,1.8) ;
-			c.string="____[t(n)/n^1.8]____" ;
-
-		}else if(tipo==CONST){
-			c.resultado= n*n;
-			c.string="[t(n)/n^2]____" ;
-
-		}else if(tipo==SOBR){
-			c.resultado=pow(n,2.2) ;
-			c.string="[t(n)/n^2.2]____" ;
-		}
-
-	}else{
-
-		if(tipo==SUBS){
-			c.resultado=pow(n,1.8);
-			c.string="___[t(n)/n^1.8]___";
-
-		}else if(tipo==CONST){
-			c.resultado= n*n ;
-			c.string= "[t(n)/n^2____";
-
-		}else if(tipo==SOBR){
-			c.resultado= pow(n,2.2);
-			c.string= "t(n)/n^2.2____";
-			
-			
-		}
-
-		
-
-	}
-	return c;
-
-
-}
-void printTablasTiempos(){
 	int i;
-	float j=247.03, k=0.003425;
+	printf("\nTest ordenación por inserción");
 	printf("\n\t   n\t\tascendente\t\tdescendente\t\tdesordenado\n(*)");
-	printf("%9d%22.2f%25.6f%24.6f\n", 500,j,k,k);
+	printf("%9d%22.2f%25.6f%24.6f\n", 500,tiempo(ord_ins,asc,500),tiempo(ord_ins,desc,500),tiempo(ord_ins,aleat,500));
 	for (i = 1000; i <= 32000; i=i*2)
 	{
-		printf("%12d%22.2f%25.6f%24.6f\n", i,j,k,k);
+		desc=malloc(i * sizeof(int));
+		asc=malloc(i * sizeof(int));
+		aleat=malloc(i * sizeof(int));
+		descendente(desc, i);
+		ascendente(asc, i);
+		aleatorio(aleat, i);
+
+		printf("%12d%22.2f%25.6f%24.6f\n", i,tiempo(ord_ins,asc,i),tiempo(ord_ins,desc,i),tiempo(ord_ins,aleat,i));
+	}
+}
+
+void printTablasTiemposOrdRap(){
+	int * desc, * asc, * aleat;
+
+	desc=malloc(500 * sizeof(int));
+	asc=malloc(500 * sizeof(int));
+	aleat=malloc(500 * sizeof(int));
+	descendente(desc, 500);
+	ascendente(asc, 500);
+	aleatorio(aleat, 500);
+
+
+	int i;
+	printf("\nTest ordenación rápida");
+	printf("\n\t   n\t\tascendente\t\tdescendente\t\tdesordenado\n(*)");
+	printf("%9d%22.2f%25.6f%24.6f\n", 500,tiempo(ord_rap,asc,500),tiempo(ord_rap,desc,500),tiempo(ord_rap,aleat,500));
+	for (i = 1000; i <= 32000; i=i*2)
+	{
+		desc=malloc(i * sizeof(int));
+		asc=malloc(i * sizeof(int));
+		aleat=malloc(i * sizeof(int));
+		descendente(desc, i);
+		ascendente(asc, i);
+		aleatorio(aleat, i);
+
+		printf("%12d%22.2f%25.6f%24.6f\n", i,tiempo(ord_rap,asc,i),tiempo(ord_rap,desc,i),tiempo(ord_rap,aleat,i));
 	}
 }
 
 
-void printTablasComplejidad(){
-	int i;
-	float j=247.03, k=0.003425;
+void printTablasComplejidadIns(){
+	int * desc, * asc, * aleat, i;
+	double k=0 , j=0;
+
+	desc=malloc(500 * sizeof(int));
+	asc=malloc(500 * sizeof(int));
+	aleat=malloc(500 * sizeof(int));
+	descendente(desc, 500);
+	ascendente(asc, 500);
+	aleatorio(aleat, 500);
 
 	printf("\n\nOrdenación por inserción con inicialización ascendente\n");
-	printf("\t   n\t\tt(n)\t\tt(n)/n^1.8\t\tt(n)/n^2\t\tt(n)/n^2.2\n(*)");
-	printf("%9d%16.2f%22.6f%22.6f%26.6f\n", 500,j,k,k,k);
+	printf("\t   n\t\tt(n)\t\tt(n)/n^0.8\t\tt(n)/n\t\tt(n)/n^1.2\n(*)");
+	printf("%9d%16.2f%22.6f%22.6f%26.6f\n", 500,tiempo(ord_ins,asc,500),tiempo(ord_ins,asc,500)/pow(500,0.8),tiempo(ord_ins,asc,500)/500,tiempo(ord_ins,asc,500)/pow(500,1.2));
 
 	for (i = 1000; i <= 32000; i=i*2)
 	{
-		printf("%12d%16.2f%22.6f%22.6f%26.6f\n", i,j,k,k,k);
+		free(asc);
+		asc=malloc(i * sizeof(int));
+		ascendente(asc, i);
+
+		printf("%12d%16.2f%22.6f%22.6f%26.6f\n", i,tiempo(ord_ins,asc,i),tiempo(ord_ins,asc,i)/pow(i,0.8),tiempo(ord_ins,asc,i)/i,tiempo(ord_ins,asc,i)/pow(i,1.2));
 	}
 
 	printf("\n\nOrdenación por inserción con inicialización descendente\n");
 	printf("\t   n\t\tt(n)\t\tt(n)/n^1.8\t\tt(n)/n^2\t\tt(n)/n^2.2\n(*)");
-	printf("%9d%16.2f%22.6f%22.6f%26.6f\n", 500,j,k,k,k);
+	printf("%9d%16.2f%22.6f%22.6f%26.6f\n", 500,j,tiempo(ord_ins,desc,500),tiempo(ord_ins,desc,500)/pow(500,1.8),tiempo(ord_ins,desc,500)/pow(500,2),tiempo(ord_ins,desc,500)/pow(500,2.2));
 	for (i = 1000; i <= 32000; i=i*2)
 	{
-		printf("%12d%16.2f%22.6f%22.6f%26.6f\n", i,j,k,k,k);
+		free(desc);
+		desc=malloc(i * sizeof(int));
+		ascendente(desc, i);
+		printf("%12d%16.2f%22.6f%22.6f%26.6f\n", i,tiempo(ord_ins,desc,i),tiempo(ord_ins,desc,i)/pow(i,1.8),tiempo(ord_ins,desc,i)/pow(i,2),tiempo(ord_ins,desc,i)/pow(i,2.2));
 	}
 
 	printf("\n\nOrdenación por inserción con inicialización desordenada\n");
 	printf("\t   n\t\tt(n)\t\tt(n)/n^1.8\t\tt(n)/n^2\t\tt(n)/n^2.2\n(*)");
-	printf("%9d%16.2f%22.6f%22.6f%26.6f\n", 500,j,k,k,k);
+	printf("%9d%16.2f%22.6f%22.6f%26.6f\n", 500,j,tiempo(ord_ins,aleat,500),tiempo(ord_ins,aleat,500)/pow(500,1.8),tiempo(ord_ins,aleat,500)/pow(500,2),tiempo(ord_ins,aleat,500)/pow(500,2.2));
 	for (i = 1000; i <= 32000; i=i*2)
 	{
-		printf("%12d%16.2f%22.6f%22.6f%26.6f\n", i,j,k,k,k);
+		free(aleat);
+		aleat=malloc(i * sizeof(int));
+		ascendente(aleat, i);
+		printf("%12d%16.2f%22.6f%22.6f%26.6f\n", i,tiempo(ord_ins,aleat,i),tiempo(ord_ins,aleat,i)/pow(i,1.8),tiempo(ord_ins,aleat,i)/pow(i,2),tiempo(ord_ins,aleat,i)/pow(i,2.2));
 	}
 }
 
@@ -468,8 +365,6 @@ void printTablasComplejidad(){
 int main(){
 	int tam = 10;
 
-
-    //Ejecución del test
 	inicializar_semilla();
 	printf("Ordenacion por insercion\n");
     test(ord_ins, tam);
@@ -479,12 +374,8 @@ int main(){
     test(ord_rap,tam);
     printf("\n");
 
-    //Ejecucion  de los tiempos
-    printTablasTiempos();
-    printTablasComplejidad();
-
-
-	
-
+	printTablasTiemposInsercion();
+	//printTablasTiemposOrdRap();
+    printTablasComplejidadIns();
 
 }
