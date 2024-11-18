fc9d8a78e5ce87708d54ad51559c886527d9679c
Xabi <xabierguitianlopez@gmail.com>
2024-11-18 16:39:48 +0100

    Comienzo p4

--- P4/p4.c
+++ P4/p4.c
@@ -11,3 +11,36 @@ void insertarMonticulo(pmonticulo m, int x);
 void quitarMenor(pmonticulo m);
 int consultarMenor(const pmonticulo m);
 void crearMonticulo(pmonticulo m, int v [], int n);
+void flotar(pmonticulo m, int i);
+
+
+
+void iniMonticulo(pmonticulo m){
+    m->ultimo=0;
+}
+
+void crearMonticulo(pmonticulo m, int v [], int n){
+    iniMonticulo(m);
+    return m;
+}
+
+void insertarMonticulo(pmonticulo m, int x){
+    if (m->ultimo >= TAM)
+    {
+        perror("Montículo lleno");
+    }else{
+        m->ultimo += 1;
+        m->vector[m->ultimo] = x;
+        flotar(m,m->ultimo);
+    }
+    
+}
+
+void flotar(pmonticulo m, int i){
+    while (i>1 && m->vector[i/2] < m->vector[i])
+    {
+        //intercambiar(m->vector[i/2], m->vector[i]);
+        i = i/2;
+    }
+    
+}
